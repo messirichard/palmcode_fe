a2d845363d758e6bfc1bd45e69610c203cdac5c0
messirichard <>
2024-05-09 04:33:40 +0700

    Add InputCountryForm and SliderForm components
    These new components InputCountryForm and SliderForm will provide better functionality for selection and input tasks respectively. The InputCountryForm will allow users to select countries with an autocomplete feature, while the SliderForm offers a graphical slider input for numeric values. These enhance user experience and facilitate input handling in forms.

---
+++ component/slider/sliderForm.jsx
@@ -0,0 +1,18 @@
+//
+// export default function sliderForm(){
+//     return(
+//         <>
+//USING LISTBOX
+//             <section className="rangeWrap">
+//                 <input type="range" list="ticks" min="0" value="0" max="10"/>
+//                 <datalist id="ticks">
+//                     <option value="0" label="0%"></option>
+//                     <option value="25" label="25%"></option>
+//                     <option value="50" label="50%"></option>
+//                     <option value="75" label="75%"></option>
+//                     <option value="100" label="100%"></option>
+//                 </datalist>
+//             </section>
+//         </>
+//     )
+// }
