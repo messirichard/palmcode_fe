b7d8a08812c6e553a59e13f7e2281c171aabe450
messirichard <>
2024-05-09 03:16:27 +0700

    Update InputForm styles and CSS; remove unused textFieldForm.jsx
    Updated styling for InputForm.jsx, improving UI aesthetics and user experience. Made significant improvements to custom.scss, customs.css, and customs.css.map to enhance appearance. Removed redundant textFieldForm.jsx from project.

--- component/card/submission/submission1.jsx
+++ component/card/submission/submission1.jsx
@@ -3,10 +3,9 @@
 import ButtonForm from "../../button/buttonForm";
 import InputForm from "@/component/textField/InputForm";
 
+const classButtons = "rounded-none text-base px-14 py-6 bg-white hover:bg-grey-300 text-black/90 dark:text-black/90";
 export default function Submission1(props) {
     const {nextStep} = props;
-    const classInputs = "hover:bg-gray-900 bg- border-none rounded-none bg-gray-900 text-white/90 dark:text-white/90 placeholder:text-default-700/50 dark:placeholder:text-white/60";
-    const classButtons = "rounded-none text-base px-14 py-6 bg-white hover:bg-grey-300 text-black/90 dark:text-black/90";
 
     return (
         <>
@@ -18,18 +17,18 @@ export default function Submission1(props) {
             <div className="form-default">
                 <div className="flex gap-7">
                     <div className="form-group flex-auto">
-                        <InputForm label={"Name"} className={classInputs} type={"text"} onChange={() => {}} />
+                        <InputForm label={"Name"} type={"text"} onChange={() => {}} />
                     </div>
                     <div className="form-group flex-auto">
-                        <InputForm label={"Country"} className={classInputs} type={"text"}  onChange={() => {}} />
+                        <InputForm label={"Country"} type={"text"}  onChange={() => {}} />
                     </div>
                 </div>
                 <div className="flex gap-7">
                     <div className="form-group flex-auto">
-                        <InputForm label={"Email"} className={classInputs} type={"email"} onChange={() => {}} />
+                        <InputForm label={"Email"} type={"email"} onChange={() => {}} />
                     </div>
                     <div className="form-group flex-auto">
-                        <InputForm label={"Whatsapp Number"} className={classInputs} type={"text"} onChange={() => {}} />
+                        <InputForm label={"Whatsapp Number"} type={"text"} onChange={() => {}} />
                     </div>
                 </div>
             </div>
--- component/textField/InputForm.jsx
+++ component/textField/InputForm.jsx
@@ -1,33 +1,33 @@
 'use client';
 import { Input } from "@nextui-org/react";
 
-
 export default function InputForm(props) {
+    const {type, label, onChange} = props;
+    let descriptionWarning = "";
+    if (type === "email") {
+        descriptionWarning = "Please include valid email"
+    }
     return (
         <>
             <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
                 <Input
-                    type={props.type}
-                    label={props.label}
-                    // color="default"
-                    // className={props.className}
+                    isClearable
+                    type={type}
+                    label={label}
                     classNames={{
-                    label: "text-white", 
-                    base: "bg-red-400",
-                    //behind the input
-                    inputWrapper: "bg-purple-500",
-                    // 
-                    innerWrapper: "bg-blue-200",
-                    // innerWrapper inside inputWrapper
+                    label: "text-white",
+                    base: "bg-gray-800",
+                    inputWrapper: ["backdrop-blur-xl", "shadow-xl","bg-black-800 ", "backdrop-saturate-900", "hover:bg-gray-800", "group-data-[focused=true]:bg-gray-800"],
+                    innerWrapper: "bg-gray-800",
                     input: [
-                        "bg-green-500",
-                        "text-green-100",
-                        "placeholder:text-yellow-500",
+                        "bg-gray-800",
+                        "text-white",
                     ],
-                    // input tag inside innerWrapper
-                    }}
-                    onChange={props.onChange}
-                    radius="none" />
+                    }}
+                    onChange={onChange}
+                    radius="none"
+                    errorMessage={descriptionWarning}
+                />
             </div>
         </>
     );
