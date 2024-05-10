dc56358eb1ea447e43a3af75a48f6cd7d0ba01cf
messirichard <>
2024-05-11 04:42:23 +0700

    Implement form validation in submission component
    Added form field validation in the submission component of the front-end. This includes checking for required fields such as name, email, whatsapp number, and country. Empty fields will show a "*Required Fields" error message, improving user experience and data integrity.

--- component/card/submission/submission1.jsx
+++ component/card/submission/submission1.jsx
@@ -4,6 +4,7 @@ import InputCountryForm from "@/component/input/inputCountryForm";
 
 import {useEffect, useState} from "react";
 import {postSubmissionStep1API} from "@/services/submission";
+import {isNull} from "@/util/util";
 
 const classButtons = "rounded-none text-base px-14 py-6 bg-white hover:bg-grey-300 text-black/90 dark:text-black/90";
 
@@ -14,21 +15,38 @@ export default function Submission1(props) {
     const [whatsapp, setWhatsapp] = useState("");
     const [idCountry, setIdCountry] = useState("")
     const [data, setData] = useState({})
-    const [dataLocal, setDataLocal] = useState({})
+    const [fieldRequiredName, setFieldRequiredName] = useState(false)
+    const [fieldRequiredEmail, setFieldRequiredEmail] = useState(false)
+    const [fieldRequiredWhatsapp, setFieldRequiredWhatsapp] = useState(false)
+    const [fieldRequiredCountry, setFieldRequiredCountry] = useState(false)
 
     useEffect(() => {
         setData({name, email, whatsapp_number:whatsapp, id_country: idCountry})
     }, [name, email, whatsapp, idCountry]);
 
     const handleClick = async () => {
-        const result = await postSubmissionStep1API(data);
-        localStorage.setItem("token", result.data.token)
 
-        if (result.data.message === "User Already Exists") {
-            nextStep(4)
-        } else {
-            nextStep(2)
+        setFieldRequiredName(!name ? true : false)
+        setFieldRequiredEmail(!email ? true : false)
+        setFieldRequiredWhatsapp(!whatsapp ? true : false)
+        setFieldRequiredCountry(!idCountry ? true : false)
+
+        if (!name && !email && !whatsapp && !idCountry) {
+            const result = await postSubmissionStep1API(data);
+            localStorage.setItem("token", result.data.token)
+
+            if (result.data.message === "User Already Exists") {
+                nextStep(4)
+            } else {
+                nextStep(2)
+            }
         }
+    }
+
+    const textErrorRequired = () => {
+        return (
+            <p className="text-red-500 text-sm">*Required Fields</p>
+        )
     }
 
     return (
@@ -37,17 +55,21 @@ export default function Submission1(props) {
                 <div className="flex gap-7">
                     <div className="form-group flex-auto w-2/4">
                         <InputForm label={"Name"} type={"text"} value={name} onChange={setName} />
+                        {fieldRequiredName && textErrorRequired()}
                     </div>
                     <div className="form-group flex-auto w-2/4">
                         <InputCountryForm setIdCountry={setIdCountry} value={idCountry} countryData={countryData}/>
+                        {fieldRequiredCountry && textErrorRequired()}
                     </div>
                 </div>
                 <div className="flex gap-7">
                     <div className="form-group flex-auto">
                         <InputForm label={"Email"} type={"email"} value={email} onChange={setEmail} />
+                        {fieldRequiredEmail && textErrorRequired()}
                     </div>
                     <div className="form-group flex-auto">
                         <InputForm label={"Whatsapp Number"} type={"text"} value={whatsapp} onChange={setWhatsapp} />
+                        {fieldRequiredWhatsapp && textErrorRequired()}
                     </div>
                 </div>
             </div>
