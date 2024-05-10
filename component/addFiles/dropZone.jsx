85a63239f00182ef29d71aaf395f2a758a1046aa
messirichard <>
2024-05-10 08:06:18 +0700

    Updated DropZone component and modified data handling in Submission3
    Enhanced the DropZone component by adding support for file type restrictions and giving the ability to remove uploaded files. Updated the Submission3 component to set and update form data based on file uploads. Refactored the file and data handling to create a seamless user-upload experience. The state handling improvement provides smoother interaction and better control to users for their uploads.

--- component/addFiles/dropZone.jsx
+++ component/addFiles/dropZone.jsx
@@ -1,76 +1,85 @@
 import {useDropzone} from 'react-dropzone';
-import {useState} from "react";
-import {useEffect} from "react";
+import {useCallback, useEffect, useState} from "react";
 import Image from 'next/image'
 
 import iconFiles from "../../assets/images/File.png"
 import deleteIcons from "../../assets/images/Icon-delete.png"
 
 export default function DropZone(props) {
-  const [uploaded, setUploaded] = useState(false)
-  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
-  
-  const onChanges = () => {
-    setUploaded(!uploaded)
-  }
-  
-  const sectionUpload = () => {
+    const {setFiles} = props
+    const [uploaded, setUploaded] = useState(false)
+    const maxSize = 2000000;
+
+    const {acceptedFiles, getRootProps, getInputProps, isDragReject, fileRejections } = useDropzone({
+        accept: {
+            'image/jpeg': [],
+            'image/png': [],
+            'image/jpg': []
+        },
+        maxSize,
+    });
+
+    // const isFileTooLarge = fileRejections.length === 0 && fileRejections[0].file.size > maxSize;
+    // console.log(isFileTooLarge)
+    const onChanges = () => {
+        setUploaded(!uploaded)
+    }
+
+    const onRemove = () => {
+        setUploaded(false)
+        setFiles([])
+    }
+
+    useEffect(() => {
+        if (acceptedFiles.length > 0) {
+            setFiles(acceptedFiles)
+            onChanges()
+        }
+    }, [acceptedFiles])
+
+    const sectionUpload = () => {
+        return (
+            <>
+                <div className='outerListNames'>
+                    <div className="items">
+                        <div className='w-1/12'>
+                            <Image src={iconFiles} alt="iconFiles" width={24} height={24}/>
+                        </div>
+                        <div className='w-10/12 descriptionFiles'>
+                            <p className='m-0'>{acceptedFiles[0].path}</p>
+                            <small>{parseFloat((acceptedFiles[0].size) / 1024).toFixed(2)}KB</small>
+                        </div>
+                        <div>
+                            <button
+                                className="del-items"
+                                onClick={() => onRemove()}
+                            >
+                                <Image src={deleteIcons} alt="iconFiles" width={20} height={19}/>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
     return (
-      <>
-        <div className='outerListNames'>
-            <div className="items">
-                <div className='w-1/12'>
-                <Image src={iconFiles} alt="iconFiles" width={24} height={24} />
-                </div>
-                <div className='w-10/12 descriptionFiles'>
-                    <p className='m-0'>{acceptedFiles[0].path}</p>
-                    <small>{parseFloat((acceptedFiles[0].size) / 1024).toFixed(2)}KB</small>
-                </div>
-                <div>
-                    <button
-                     className="del-items"
-                     onClick={() => onChanges()}
-                     >
-                        <Image src={deleteIcons} alt="iconFiles" width={20} height={19} />
-                        </button>
-                </div>
-            </div>
-        </div>
-      </>
-    )
-  }
-
-  useEffect(() => {
-    if(acceptedFiles.length > 0){
-      onChanges()
-    }
-  },[acceptedFiles])
-
-  console.log(acceptedFiles);
-  
-  const files = acceptedFiles.map(file => (
-      <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
-
-  return (
-    <>
-    {uploaded ?
-        sectionUpload()
-    : 
-    <>
-        <section className="outer-drop-zone"
-        >
-        <div {...getRootProps({className: 'dropzone'})}>
-            <input {...getInputProps()} />
-            <h4>Drag & Drop</h4>
-            <p>or select files from device max. 2MB</p>
-            <h6>Upload</h6>
-        </div>
-        </section>
-    </>
-    }
-    </>
-  );
+        <>
+            {uploaded ?
+                sectionUpload()
+                :
+                <>
+                    <section className="outer-drop-zone"
+                    >
+                        <div {...getRootProps({className: 'dropzone'})}>
+                            <input {...getInputProps()} />
+                            <h4>Drag & Drop</h4>
+                            <p>or select files from device max. 2MB</p>
+                            <h6>Upload</h6>
+                        </div>
+                    </section>
+                </>
+            }
+        </>
+    );
 }
--- component/card/submission/submission3.jsx
+++ component/card/submission/submission3.jsx
@@ -5,19 +5,26 @@ import InputDate from "@/component/input/inputDate";
 import InputVariant from "@/component/input/inputVariant";
 import DropZone from "@/component/addFiles/dropZone";
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const classButtons = "rounded-none text-base px-14 py-6 bg-white hover:bg-grey-300 text-black/90 dark:text-black/90";
 
 export default function Submission3(props) {
     const {nextStep} = props;
-    
+    const [data, setData] = useState([])
+    const [files, setFiles] = useState([])
+
+    useEffect(() => {
+        setData({imageFile: files})
+    }, [files]);
+
+    console.log(data)
     return (
        <>
         <p>Help us verify your identity by uploading a photo of your ID/KTP or Passport</p>
             <div className="form-default">
                 <div className="pt-5 pb-7">
-                    <DropZone />
+                    <DropZone setFiles={setFiles}/>
                 </div>
             </div>
             <div className="block-buttons">
